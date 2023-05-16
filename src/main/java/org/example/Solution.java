047a85648c8a6511ea65d73a26a9822fb4157eff
alek <wait82985@gmail.com>
2023-05-16 19:08:59 +0600

    problem solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        return 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        //base cases
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = matrix[0][j];
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (j == 0) {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j];
+                } else if (j == matrix[i].length - 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + matrix[i][j];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i - 1][j + 1]) + matrix[i][j];
+                }
+            }
+        }
+        Arrays.sort(dp[matrix.length - 1]);
+        return dp[dp.length - 1][0];
     }
+
 }
